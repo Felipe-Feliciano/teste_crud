d3a1dac0e19b589b6cccbcc76cd1a953fdeeb79f
Felipe-Feliciano <149983440+Felipe-Feliciano@users.noreply.github.com>
2023-11-05 23:14:22 -0300

    Add files via upload

---
+++ testeCRUD/config.php
@@ -0,0 +1,10 @@
+<?php 
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', 'root');
+    define('BASE', 'kbr_cadastro');
+
+    $conn = new mysqli(HOST, USER, PASS, BASE);
+    
+
+?>
